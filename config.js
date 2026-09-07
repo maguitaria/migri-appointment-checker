0c0a9f759095cfc589a0a735be9d85045d3e81f2
Maria Glushenkova <mariia.glushenkova@tieto.com>
2026-09-07 13:14:43 +0300

    Point website to the real Telegram bot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MIGRI_CONFIG = {
   API_URL: 'https://migri-appointment-api.mglushen22.workers.dev',
-  TELEGRAM_BOT_USERNAME: 'MyBlogNotifierBot'
+  TELEGRAM_BOT_USERNAME: 'MyBlogNotifier_maguitaria_Bot'
 }
